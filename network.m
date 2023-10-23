2b98191e59e6d6395b6ba162525176897294a914
Godesist <56435361+Godesist@users.noreply.github.com>
2023-10-23 21:57:31 +0300

    created

---
+++ network.m
@@ -0,0 +1,50 @@
+% leveling network
+% data: coefficient, heights, observations for the leveling network
+
+% coefficient matrix
+Coeff=[1 0 0 0 -1 0 0
+    1 0 0 0 0 -1 0
+    1 -1 0 0 0 0 0
+    0 -1 0 0 0 1 0
+    0 -1 0 0 0 0 1
+    0 1 -1 0 0 0 0
+    0 0 -1 0 0 0 1
+    0 0 -1 1 0 0 0
+    0 0 0 1 0 0 -1
+    0 0 0 1 0 -1 0
+    0 0 0 1 -1 0 0
+    0 0 0 0 -1 1 0
+    0 0 0 0 0 -1 1
+    -1 0 0 0 0 0 1
+    0 0 0 0 -1 0 1];
+
+% heights
+H=[104
+    102.4
+    100.72
+    105.21
+    101.15
+    103.3
+    104.13];
+
+[nn uu]=size(Coeff);
+
+Olculer=Coeff*H; % observations
+
+% leveling lines in km
+ghetero=[0.920
+    1.254
+    1.602
+    0.987
+    1.210
+    1.320
+    0.895
+    0.959
+    1.302
+    0.842
+    0.801
+    1.021
+    1.305
+    1.485
+    1.387];
+
